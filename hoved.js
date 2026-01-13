c797fbb6d03c19b629ea78f002e9cd88ec632af6
pigk1402-ctrl <pigk1402@gmail.com>
2026-01-13 13:23:45 +0100

    website improving 2

--- hoved.js
+++ hoved.js
@@ -1,16 +1,27 @@
-const hamburgerIcon = document.querySelector(".dropdown");
-const titleMain = document.querySelector(".titlemain");
+// const hamburgerIcon = document.querySelector(".dropdown");
+// const titleMain = document.querySelector(".titlemain");
 
-function grayScale100() {
-  titleMain.style.filter = "grayscale(100%)";
-}
-function grayScale0() {
-  titleMain.style.filter = "grayscale(0%)";
-}
-//hamburgerIcon.addEventListener("mouseover", myFunction);
+// function grayScale100() {
+//   titleMain.style.filter = "grayscale(100%)";
+// }
+// function grayScale0() {
+//   titleMain.style.filter = "grayscale(0%)";
+// }
+// //hamburgerIcon.addEventListener("mouseover", myFunction);
 
-if (hamburgerIcon.matches(":hover")) {
-  grayScale100();
-} else {
-  grayScale0();
+// if (hamburgerIcon.matches(":hover")) {
+//   grayScale100();
+// } else {
+//   grayScale0();
+// }
+
+const dropdowmBtnElm = document.getElementById("dropdown-btn");
+const dropdownContentElm = document.getElementById("dropdown-content");
+
+function toggleDropdown() {
+  if (dropdownContentElm.style.display) {
+    dropdownContentElm.style.display = "";
+  } else {
+    dropdownContentElm.style.display = "block";
+  }
 }
